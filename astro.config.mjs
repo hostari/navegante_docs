6ded4c18e41bbb0dfe967855bdedfb1008b1ea38
Richard Parayno <broodheart@gmail.com>
2026-02-10 19:30:38 +0800

    updated astro config and initial quick start draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,77 +4,103 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.docs-test.hostari-labs.com",
-  server: {
-    allowedHosts: true,
-    host: true,
-  },
-  integrations: [
-    starlight({
-      title: "Navegante Docs",
-      social: [
-        {
-          icon: "x.com",
-          label: "x.com",
-          href: "https://x.com/navegantedotapp",
-        },
-      ],
-      sidebar: [
-        {
-          label: "Start Here",
-          items: [
-            {
-              label: "Welcome to Navegante",
-              slug: "welcome",
-            },
-            {
-              label: "Quick Start",
-              slug: "quick-start",
-            },
-          ],
-        },
-        {
-          label: "Features",
-          autogenerate: { directory: "features" },
-        },
-        {
-          label: "Framework Guides",
-          autogenerate: { directory: "framework-guides" },
-        },
-        {
-          label: "Architecture",
-          autogenerate: { directory: "architecture" },
-        },
-        {
-          label: "Reference",
-          items: [
-            {
-              label: "Infrastructure Projects Reference",
-              slug: "reference/infrastructure-projects",
-            },
-            {
-              label: "Application Configurations Reference",
-              slug: "reference/application-configurations",
-            },
-            {
-              label: "Application Services Reference",
-              slug: "reference/application-services",
-            },
-            {
-              label: "Application Deployments Reference",
-              slug: "reference/application-deployments",
-            },
-            {
-              label: "Organizations Reference",
-              slug: "reference/organizations",
-            },
-            {
-              label: "Role-Based Access Control (RBAC) Reference",
-              slug: "reference/organizations",
-            },
-          ],
-        },
-      ],
-    }),
-  ],
+    site: "https://docs.navegante.app",
+    server: {
+        allowedHosts: true,
+        host: true,
+    },
+    integrations: [
+        starlight({
+            title: "Navegante Docs",
+            social: [
+                {
+                    icon: "x.com",
+                    label: "x.com",
+                    href: "https://x.com/navegantedotapp",
+                },
+            ],
+            sidebar: [
+                {
+                    label: "Start Here",
+                    items: [
+                        {
+                            label: "Welcome to Navegante",
+                            slug: "welcome",
+                        },
+                        {
+                            label: "Quick Start",
+                            slug: "quick-start",
+                        },
+                    ],
+                },
+                {
+                    label: "Features",
+                    autogenerate: { directory: "features" },
+                },
+                {
+                    label: "Framework Guides",
+                    autogenerate: { directory: "framework-guides" },
+                },
+                {
+                    label: "Deployment Quirks",
+                    items: [
+                        {
+                            label: "Static Site Generators",
+                            slug: "",
+                        },
+                        {
+                            label: "Javascript-based Frameworks",
+                            slug: "",
+                        },
+                    ],
+                },
+                {
+                    label: "Architecture",
+                    autogenerate: { directory: "architecture" },
+                },
+                {
+                    label: "Github App",
+                    items: [
+                        {
+                            label: "Github Integration",
+                            slug: "",
+                        },
+                        {
+                            label: "Troubleshooting",
+                            slug: "",
+                        },
+                    ],
+                },
+                {
+                    label: "Reference",
+                    items: [
+                        {
+                            label: "Infrastructure Projects Reference",
+                            slug: "reference/infrastructure-projects",
+                        },
+                        {
+                            label: "Application Configurations Reference",
+                            slug: "reference/application-configurations",
+                        },
+                        {
+                            label: "Application Services Reference",
+                            slug: "reference/application-services",
+                        },
+                        {
+                            label: "Application Deployments Reference",
+                            slug: "reference/application-deployments",
+                        },
+                        {
+                            label: "Organizations Reference",
+                            slug: "reference/organizations",
+                        },
+                        {
+                            label: "Role-Based Access Control (RBAC) Reference",
+                            slug: "reference/organizations",
+                        },
+                    ],
+                },
+            ],
+        }),
+    ],
 });
--- src/content/docs/quick-start.md
+++ src/content/docs/quick-start.md
@@ -3,9 +3,60 @@ title: Quick Start
 description: Get started with Navegante and deploy your app in 10 minutes.
 ---
 
-Guides lead a user through a specific task they want to accomplish, often with a sequence of steps.
-Writing a good guide requires thinking about what your users are trying to do.
+Getting started with Navegante is easy. Deploy your first app in minutes by following the steps below.
 
-## Further reading
+[ Add Image or Screen Recording here ]
 
-- Read [about how-to guides](https://diataxis.fr/how-to-guides/) in the Diátaxis framework
+### 1. Prerequisites
+
+To use start using Navegante, you need to have:
+
+1. An active Github Account\* with public or private repositories.
+
+_\*We only support Github for now._
+
+### 2. Create your free Navegante Account
+
+Creating a Navegante account is free and easy to do. Visit [ insert signup link here ] to create your account.
+
+All Navegante accounts (free and paid) are entitled to 1 free Infrastructure Project (Hobby Plan). Learn more about our free-forever Hobby plan [ here ].
+
+### 3. Install the Navegante Github App
+
+Navegante relies on Github to securely pull, build, and deploy your software to the Internet.
+
+Make sure to install the Github App and provide access to your desired personal or organization repos. Read the [ Github guide link ] to learn more about our GitHub integration.
+
+### 4. Create your first Infrastructure Project
+
+Infrastructure projects are collections made up of Github repos that you want to deploy.
+
+To create an Infrastructure Project:
+
+1. Click on the "Create a New Infrastructure Project" button.
+2. Write the desired name of your Infrastructure Project (or leave it blank for an auto-generated name).
+3. Select the desired Compute Plan for your Infrastructure Project.
+4. Select the repos you want to add to your Infrastructure Project.
+5. Create and view your project in the dashboard.
+
+Infrastructure Projects are billed according to the Compute Plan you selected during the project creation process. If you wish to update your Compute Plan, reach out to us at `support@navegante.app`.
+
+Click [ here ] to learn more about Infrastructure Projects.
+
+### 5. Configure your Infrastructure Project
+
+Insert section for configuration here
+
+### 6. Deploy!
+
+If you followed the instructions...
+
+## Check out our deployment guides!
+
+Now that your Navegante account is ready for deployments, you might want to read up on some of our deployment guides.
+
+[ insert list of deployment guides ]
+
+Certain frameworks need some manual configuration on Navegante to be deployed.
+
+For JS-based frameworks and static site generators, we recommend going through the [ insert link for Javascript FAQs ] before deploying on Navegante to ensure a smooth experience.
